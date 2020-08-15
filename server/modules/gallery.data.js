90264ffd3e452da8e3efa0f6b0ec128dcd40710e
Jenni Schubert <jenni.schubert@gmail.com>
2020-08-15 08:03:08 -0500

    updated get for sql

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,13 +1,13 @@
-const galleryItems = [
-    { id: 1, path: 'images/redwoods.jpeg', description: 'Friend trip to Redwoods National Park this February.', likes: 0 },
-    { id: 2, path: 'images/bowl.jpeg', description: 'A bowl I made!', likes: 0 },
-    { id: 3, path: 'images/cascade.jpeg', description: 'My husband and I take an annual North Shore camping trip', likes: 0 },
-    { id: 4, path: 'images/colorado.jpeg', description: 'Friend trip to Colorado in 2018', likes: 0 },
-    { id: 5, path: 'images/kayak.jpeg', description: 'Goose on her first kayak trip at the cabin', likes: 0 },
-    { id: 6, path: 'images/kidphoto.jpeg', description: 'The only kid photo I have a digital copy of!', likes: 0 },
-    { id: 7, path: 'images/parents.jpeg', description: "Parents + husband at Psycho Suzi's", likes: 0 },
-    { id: 8, path: 'images/wedding2.jpeg', description: 'Me + husband Rob on our wedding day', likes: 0 },
+// const galleryItems = [
+//     { id: 1, path: 'images/redwoods.jpeg', description: 'Friend trip to Redwoods National Park this February.', likes: 0 },
+//     { id: 2, path: 'images/bowl.jpeg', description: 'A bowl I made!', likes: 0 },
+//     { id: 3, path: 'images/cascade.jpeg', description: 'My husband and I take an annual North Shore camping trip', likes: 0 },
+//     { id: 4, path: 'images/colorado.jpeg', description: 'Friend trip to Colorado in 2018', likes: 0 },
+//     { id: 5, path: 'images/kayak.jpeg', description: 'Goose on her first kayak trip at the cabin', likes: 0 },
+//     { id: 6, path: 'images/kidphoto.jpeg', description: 'The only kid photo I have a digital copy of!', likes: 0 },
+//     { id: 7, path: 'images/parents.jpeg', description: "Parents + husband at Psycho Suzi's", likes: 0 },
+//     { id: 8, path: 'images/wedding2.jpeg', description: 'Me + husband Rob on our wedding day', likes: 0 },
 
-];
+// ];
 
-module.exports = galleryItems;
+// module.exports = galleryItems;
--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const galleryItems = require('../modules/gallery.data');
+const pool = require('../modules/pool.js')
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
@@ -18,7 +19,15 @@ router.put('/like/:id', (req, res) => {
 
 // GET Route
 router.get('/', (req, res) => {
-    res.send(galleryItems);
+    const queryText = 'SELECT * FROM "gallery"';
+    
+    pool.query(queryText)
+    .then(result => {
+        res.send(result.rows);
+    }).catch(error => {
+        console.log('error in sql query', queryText, error);
+        res.sendStatus(500);
+    })
 }); // END GET Route
 
 module.exports = router;
